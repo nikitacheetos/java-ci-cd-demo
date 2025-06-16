eef3b2de28ef90a8d86341b944fbb49ec012375a
Nikita Titov <nikitatitov@MacBook-Pro-Nikita.local>
2025-06-16 12:21:56 +0300

    Test

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,7 +1,27 @@
+import java.util.List;
+
 public class Calculator {
+
 
     public int sum(int a, int b) {
         return a + b;
     }
 
+    public static double divideNumbers(List<Double> numbers) {
+        if (numbers == null || numbers.isEmpty()) {
+            throw new ArithmeticException("Список чисел не может быть пустым!");
+        }
+
+        double result = numbers.get(0);
+
+        for (int i = 1; i < numbers.size(); i++) {
+            double divisor = numbers.get(i);
+            if (divisor == 0) {
+                throw new ArithmeticException("Деление на ноль невозможно!");
+            }
+            result /= divisor;
+        }
+
+        return result;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        System.out.printf(String.valueOf(calculator.sum(1, 3)));
+        System.out.println(String.valueOf(calculator.sum(1, 3)));
+
+
+        List<Double> numbers = Arrays.asList(52222133131348123123123.0, 100.0, 5.0, 2.0); // 100 / 5 / 2 = 10
+        double result = Calculator.divideNumbers(numbers);
+        System.out.println("Результат: " + result); // Результат: 10.0
     }
 }
