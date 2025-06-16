928bdb8f732aa0ca0fad67d5d87275b1804b12db
Nikita Titov <nikitatitov@MacBook-Pro-Nikita.local>
2025-06-16 12:31:55 +0300

    Метод calculate

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,16 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+
         Calculator calculator = new Calculator();
         System.out.println(String.valueOf(calculator.sum(1, 3)));
-
 
         List<Double> numbers = Arrays.asList(52222133131348123123123.0, 100.0, 5.0, 2.0); // 100 / 5 / 2 = 10
         double result = Calculator.divideNumbers(numbers);
         System.out.println("Результат: " + result); // Результат: 10.0
+    }
+
+    public static int calculate (Calculator calculator){
+       return calculator.sum(1, 2);
     }
 }
